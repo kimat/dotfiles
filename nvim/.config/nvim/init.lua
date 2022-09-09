c892b1bb95dbfb681d2d7e85e4bb535a7bb7dac1
Kimat Boven <kimat@users.noreply.github.com>
2022-09-10 01:53:06 +0200

    ale -> nvim-formatter + nvim-lint + formatter.nvim + nvim-lspconfig ...
    + moving: to nvim-cmp autocompletion base on lsp
    + changing nvim lua config files required to run luafile % +
      PackerCompile to have that configuration applied
    + reformatted files using stylua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,14 +3,16 @@ api, cmd, fn, g = vim.api, vim.cmd, vim.fn, vim.g
 opt, wo = vim.opt, vim.wo
 fmt = string.format
 function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
   api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 --[[ vim.opt.setpath = ""
 vim.opt.suffixesadd = ".lua" ]]
-require('better-defaults')
-require('personal-bindings')
-require('plugins')
-require('styling')
+require "better-defaults"
+require "personal-bindings"
+require "plugins"
+require "styling"
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,149 +1,287 @@
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 if not packer_exists then
-  print("packer.nvim isn't installed")
-  print(
-      "visit https://github.com/wbthomason/packer.nvim#quickstart for clone instructions")
+  print "packer.nvim isn't installed"
+  print "visit https://github.com/wbthomason/packer.nvim#quickstart for clone instructions"
   return
 end
 
--- vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
--- vim.cmd([[autocmd BufNewFile,BufRead *.yaml setlocal syntax=yaml]])
-vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
-
 vim.cmd [[packadd packer.nvim]]
-require('packer').startup({
+require("packer").startup {
   function()
     use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
+      "nvim-treesitter/nvim-treesitter",
+      requires = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        "RRethy/nvim-treesitter-endwise",
+        "windwp/nvim-ts-autotag",
+      },
+      run = ":TSUpdate",
       config = function()
         -- set foldmethod=expr
         -- set foldexpr=nvim_treesitter#foldexpr()
         -- require'nvim-treesitter.install'.compilers = {'gcc'}
-        vim.wo.foldmethod = 'expr'
-        vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-        require'nvim-treesitter.configs'.setup {
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+        require("nvim-treesitter.configs").setup {
+          context_commentstring = { enable = true, enable_autocmd = false }, -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+          endwise = { enable = true }, -- https://github.com/RRethy/nvim-treesitter-endwise
+          autotag = { -- https://github.com/windwp/nvim-ts-autotag
+            enable = true,
+          },
           -- textobjects = {enable = true},
-          rainbow = {enable = true},
-          indent = {enable = true, disable = {}},
-          folding = {enable = true},
-          highlight = {enable = true, additional_vim_regex_highlighting = false},
+          -- rainbow = { enable = true },
+          indent = { enable = true, disable = {} },
+          folding = { enable = true },
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+          },
           -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
           ensure_installed = {
-            "dockerfile", "html", "bash", "javascript", "json", "lua", "nix",
-            "ruby", "vim", "yaml"
+            "bash",
+            "dockerfile",
+            "gitignore",
+            "hcl",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "nix",
+            "python",
+            "ruby",
+            "vim",
+            "yaml",
             -- , "markdown"
-          }
+          },
         }
-      end
+      end,
     }
 
     use {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "nvim-treesitter/nvim-treesitter-textobjects",
       config = function()
-        require'nvim-treesitter.configs'.setup {
+        require("nvim-treesitter.configs").setup {
           textobjects = {
             move = {
               enable = true,
               set_jumps = true, -- whether to set jumps in the jumplist
               goto_next_start = {
                 [",,"] = "@function.outer",
-                [",a"] = "@parameter.inner"
+                [",a"] = "@parameter.inner",
                 -- [",m"] = "@class.outer"
               },
-              goto_previous_start = {[",z"] = "@parameter.inner"}
+              goto_previous_start = { [",z"] = "@parameter.inner" },
             },
             swap = {
               enable = true,
-              swap_next = {[",l"] = "@parameter.inner"},
-              swap_previous = {[",h"] = "@parameter.inner"}
+              swap_next = { [",l"] = "@parameter.inner" },
+              swap_previous = { [",h"] = "@parameter.inner" },
             },
             select = {
               enable = true,
               lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
               keymaps = {
                 -- You can use the capture groups defined in textobjects.scm
-                ["ar"] = "@block.outer",
-                ["ir"] = "@block.inner",
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
                 ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner"
-              }
-            }
-          }
+                ["af"] = "@function.outer",
+                ["ar"] = "@block.outer",
+                ["ic"] = "@class.inner",
+                ["if"] = "@function.inner",
+                ["ir"] = "@block.inner",
+              },
+            },
+          },
         }
-      end
+      end,
     }
     if pcall(require, "nvim-treesitter.parsers") then
       -- install with ':TSInstallSync markdown'
-      require"nvim-treesitter.parsers".get_parser_configs().markdown = {
+      require("nvim-treesitter.parsers").get_parser_configs().markdown = {
         install_info = {
           url = "https://github.com/ikatyang/tree-sitter-markdown",
-          files = {"src/parser.c", "src/scanner.cc"}
-        }
+          files = { "src/parser.c", "src/scanner.cc" },
+        },
       }
     end
 
+    use {
+      "hrsh7th/nvim-cmp",
+      requires = {
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "L3MON4D3/LuaSnip" },
+      },
+      config = function()
+        local has_words_before = function()
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0
+            and vim.api
+                .nvim_buf_get_lines(0, line - 1, line, true)[1]
+                :sub(col, col)
+                :match "%s"
+              == nil
+        end
+        local luasnip = require "luasnip"
+        local cmp = require "cmp"
+        cmp.setup {
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+          mapping = {
+            ["<Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+          },
+        }
+      end,
+    }
+    use {
+      "neovim/nvim-lspconfig",
+      config = function()
+        vim.opt.completeopt = "menuone,noselect"
+        local capabilities = require("cmp_nvim_lsp").update_capabilities(
+          vim.lsp.protocol.make_client_capabilities()
+        )
+        require("lspconfig").bashls.setup { capabilities = capabilities } -- https://github.com/bash-lsp/bash-language-server
+        require("lspconfig").solargraph.setup { capabilities = capabilities } --
+        require("lspconfig").terraformls.setup { capabilities = capabilities } -- https://github.com/hashicorp/terraform-ls
+      end,
+    }
+
     -- Packer can manage itself as an optional plugin
-    use {"wbthomason/packer.nvim", opt = true}
+    use { "wbthomason/packer.nvim", opt = true }
 
     use {
-      'folke/tokyonight.nvim',
+      "folke/tokyonight.nvim",
       config = function()
         -- vim.g.tokyonight_italic_functions = false
         -- vim.g.tokyonight_style = "day"
         -- vim.g.tokyonight_transparent = true
         -- vim.cmd 'colorscheme tokyonight'
         -- vim.cmd 'hi CursorLine cterm=NONE ctermbg=lightblue guibg=lightblue'
-      end
+      end,
     }
 
     use {
-      'Th3Whit3Wolf/space-nvim',
+      "Th3Whit3Wolf/space-nvim",
       config = function()
         -- vim.o.background = "light"
         -- vim.cmd 'colorscheme space-nvim'
-      end
+      end,
     }
 
-    use {"rktjmp/lush.nvim"}
+    use { "rktjmp/lush.nvim" }
     use {
       "mcchrish/zenbones.nvim",
       config = function()
-        vim.cmd 'colorscheme zenbones'
+        vim.cmd "colorscheme zenbones"
         vim.o.background = "light"
         -- requires = "rktjmp/lush.nvim"
-      end
+      end,
     }
+
+    use { "mfussenegger/nvim-lint" }
 
     use {
-      'w0rp/ale',
+      "mhartington/formatter.nvim",
       config = function()
-        vim.g.ale_fix_on_save = 1
-        vim.g.ale_lua_lua_format_options = "--no-use-tab --indent-width=2"
-        vim.g.ruby_rubocop_options = "--safe"
-        vim.g.ale_sign_error = '✖'
-        vim.g.ale_sign_warning = '⚠'
-        vim.g.ale_sign_style_error = 'q'
-        vim.g.ale_sign_style_warning = 'S'
-
-        vim.g.ale_linters = {
-          ['ruby'] = {'rubocop'},
-          ['javascript'] = {'eslint'}
+        local util = require "formatter.util"
+        require("formatter").setup {
+          logging = true,
+          filetype = {
+            ruby = {
+              function()
+                return {
+                  exe = "rubocop",
+                  args = {
+                    "--fix-layout",
+                    "--stdin",
+                    util.escape_path(util.get_current_buffer_file_name()),
+                    "--format",
+                    "files",
+                  },
+                  stdin = true,
+                  transform = function(text)
+                    table.remove(text, 1)
+                    table.remove(text, 1)
+                    return text
+                  end,
+                }
+              end,
+            },
+            lua = {
+              function()
+                return {
+                  exe = "stylua",
+                  args = {
+                    "--search-parent-directories",
+                    "--stdin-filepath",
+                    util.escape_path(util.get_current_buffer_file_path()),
+                    "--",
+                    "-",
+                  },
+                  stdin = true,
+                  -- cf. stylua.toml for extra config
+                }
+              end,
+            },
+            terraform = {
+              function()
+                return { exe = "terraform", args = { "fmt", "-" }, stdin = true }
+              end,
+            },
+          },
         }
-
-        vim.g.ale_fixers = {
-          ['nix'] = {'nixfmt'},
-          ['lua'] = {'lua-format'},
-          ['javascript'] = {'prettier'},
-          ['javascriptreact'] = {'prettier'},
-          ['markdown'] = {'prettier'},
-          ['ruby'] = {'rubocop'}
-        }
-      end
+      end,
     }
+
+    -- use {
+    --   'dense-analysis/ale',
+    --   config = function()
+    --     vim.g.ale_fix_on_save = 1
+    --     vim.g.ale_lua_lua_format_options = "--no-use-tab --indent-width=2"
+    --     vim.g.ruby_rubocop_options = "--safe"
+    --     vim.g.ale_sign_error = '✖'
+    --     vim.g.ale_sign_warning = '⚠'
+    --     vim.g.ale_sign_style_error = 'q'
+    --     vim.g.ale_sign_style_warning = 'S'
+
+    --     vim.g.ale_linters = {
+    --       ['ruby'] = {'rubocop'},
+    --       ['javascript'] = {'eslint'}
+    --     }
+
+    --     vim.g.ale_fixers = {
+    --       ['nix'] = {'nixfmt'},
+    --       ['lua'] = {'lua-format'},
+    --       ['javascript'] = {'prettier'},
+    --       ['javascriptreact'] = {'prettier'},
+    --       ['markdown'] = {'prettier'},
+    --       ['ruby'] = {'rubocop'},
+    --       ['hcl'] = {'terraform'}
+    --     }
+    --   end
+    -- }
 
     -- use {
     --   'plasticboy/vim-markdown',
@@ -196,45 +334,68 @@ require('packer').startup({
     --     }
 
     use {
-      'b3nj5m1n/kommentary',
+      "numToStr/Comment.nvim",
       config = function()
-        require('kommentary.config').configure_language("default", {
-          prefer_single_line_comments = true
-        })
-      end
+        require("Comment").setup {
+          pre_hook = require(
+            "ts_context_commentstring.integrations.comment_nvim"
+          ).create_pre_hook(),
+        }
+      end,
     }
 
+    -- use {
+    --   "b3nj5m1n/kommentary",
+    --   config = function()
+    --     require("kommentary.config").configure_language("default", {
+    --       prefer_single_line_comments = true,
+    --     })
+    --   end,
+    -- }
+
     use {
-      'windwp/nvim-autopairs',
-      config = function() require('nvim-autopairs').setup() end
+      "windwp/nvim-autopairs",
+      config = function()
+        require("nvim-autopairs").setup()
+      end,
     }
 
     -- use 'nvim-treesitter/nvim-treesitter' -- syntax highlight using treesitter
     use {
-      'norcalli/nvim-colorizer.lua',
+      "norcalli/nvim-colorizer.lua",
       config = function()
-        require'colorizer'.setup({'css', 'javascript', 'html'})
-      end
+        require("colorizer").setup { "css", "javascript", "html" }
+      end,
     }
 
     use {
-      'ibhagwan/fzf-lua',
-      requires = {'vijaymarupudi/nvim-fzf'},
+      "ibhagwan/fzf-lua",
+      requires = { "vijaymarupudi/nvim-fzf" },
       -- 'kyazdani42/nvim-web-devicons' } -- optional for icons
       -- use {'vijaymarupudi/nvim-fzf-commands'}
       config = function()
-        map('n', 'mo', '<Cmd>FzfLua files<CR>')
-        map('n', 'ml', '<Cmd>FzfLua buffers<CR>')
-        map('n', 'mt',
-            "<Cmd>lua require('fzf-lua').files({ cwd = '~/my/tips' })<CR>")
-        map('n', 'mc',
-            "<Cmd>lua require('fzf-lua').files({ cmd = 'cat ~/my/zsh/.config/marks/configs' })<CR>")
-        map('n', 'mm', "<Cmd>lua require('fzf-lua').files({ cwd = '~/my' })<CR>")
+        map("n", "mo", "<Cmd>FzfLua files<CR>")
+        map("n", "ml", "<Cmd>FzfLua buffers<CR>")
+        map(
+          "n",
+          "mt",
+          "<Cmd>lua require('fzf-lua').files({ cwd = '~/my/tips' })<CR>"
+        )
+        map(
+          "n",
+          "mc",
+          "<Cmd>lua require('fzf-lua').files({ cmd = 'cat ~/my/zsh/.config/marks/configs' })<CR>"
+        )
+        map(
+          "n",
+          "mm",
+          "<Cmd>lua require('fzf-lua').files({ cwd = '~/my' })<CR>"
+        )
 
-        require('fzf-lua').setup {
+        require("fzf-lua").setup {
           default_previewer = "cat",
-          files = {fzf_ansi = '', file_icons = false, git_icons = false},
-          preview_layout = "vertical"
+          files = { fzf_ansi = "", file_icons = false, git_icons = false },
+          preview_layout = "vertical",
           -- TODO bat theme not working because bright?
           -- bat = {
           --   cmd    = "bat",
@@ -250,47 +411,71 @@ require('packer').startup({
           --   'ctrl-a:toggle-all', 'ctrl-l:clear-screen'
           -- }
         }
-      end
+      end,
     }
 
     -- a
 
     -- viml based plugins
     use {
-      'previm/previm',
-      config = function() vim.g.previm_open_cmd = 'firefox' end
+      "previm/previm",
+      config = function()
+        vim.g.previm_open_cmd = "firefox"
+      end,
     }
     use {
-      'esamattis/slimux',
+      "esamattis/slimux",
       config = function()
-        map('v', 'ee', ':SlimuxREPLSendSelection<CR>')
-        map('n', 'ee', '<Cmd>SlimuxREPLSendLine<CR>')
-        map('n', 'ep', 'vip:SlimuxREPLSendSelection<CR>')
-        map('n', 'ex', '<Cmd>SlimuxGlobalConfigure<CR>')
-        map('n', 'ej',
-            ':let @t=expand("%:.").":".line(".")<CR>:SlimuxShellRun spring rspec <c-r>t<CR>')
-        map('n', 'eJ',
-            ':let @t=expand("%:.")<CR>:SlimuxShellRun spring rspec <c-r>t<CR>')
+        map("v", "ee", ":SlimuxREPLSendSelection<CR>")
+        map("n", "ee", "<Cmd>SlimuxREPLSendLine<CR>")
+        map("n", "ep", "vip:SlimuxREPLSendSelection<CR>")
+        map("n", "ex", "<Cmd>SlimuxGlobalConfigure<CR>")
+        map(
+          "n",
+          "ej",
+          ':let @t=expand("%:.").":".line(".")<CR>:SlimuxShellRun spring rspec <c-r>t<CR>'
+        )
+        map(
+          "n",
+          "eJ",
+          ':let @t=expand("%:.")<CR>:SlimuxShellRun spring rspec <c-r>t<CR>'
+        )
         -- map <localleader>J :let @t=expand("%:.")<CR>:SlimuxShellRun spring rspec <c-r>t<CR>
         -- map <localleader>u :let @t=expand("%:.")<CR>:SlimuxShellRun cucumber -p local <c-r>t<CR>
         -- map <localleader>U :let @t=expand("%:.")<CR>:SlimuxShellRun cucumber -p local <c-r>t<CR>
-      end
+      end,
     }
-    use {'tpope/vim-fugitive'}
-    use {'tpope/vim-rails'}
-    use {'tpope/vim-endwise'}
-    use {'editorconfig/editorconfig-vim'}
-    use {'bogado/file-line'}
+    use { "tpope/vim-fugitive" }
+    use { "tpope/vim-rails" }
+    -- use { "tpope/vim-endwise" }
+    use { "editorconfig/editorconfig-vim" }
+    use { "bogado/file-line" }
     -- reformat text
-    use {'AndrewRadev/splitjoin.vim'}
-    use {'godlygeek/tabular'}
+    use { "AndrewRadev/splitjoin.vim" }
+    use { "godlygeek/tabular" }
 
     config = {
       display = {
         open_fn = function()
-          return require('packer.util').float({border = 'single'})
-        end
-      }
+          return require("packer.util").float { border = "single" }
+        end,
+      },
     }
-  end
+  end,
+}
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua FormatWrite | source <afile> | PackerCompile
+  augroup end
+  augroup FormatAutogroup
+    autocmd!
+    autocmd BufWritePost * FormatWrite
+  augroup END
+]]
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
 })
