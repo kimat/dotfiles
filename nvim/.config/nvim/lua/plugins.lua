4282a448b0c1bdcadd36209fbf6a7f1f6bbc51ca
Kimat Boven <kimat@users.noreply.github.com>
2023-01-09 17:52:40 +0100

    linting

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -102,6 +102,13 @@ require("packer").startup {
       "hrsh7th/nvim-cmp",
       config = function()
         require("cmp").setup {
+          -- mapping = cmp.mapping.preset.insert {
+          --   ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          --   ["<C-Space>"] = cmp.mapping.complete(),
+          --   ["<C-e>"] = cmp.mapping.abort(),
+          --   ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- },
           snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
