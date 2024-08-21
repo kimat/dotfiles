e7e7ec42fa837c3cbbf534cbe370c80b42b4b8db
Kimat Boven <kimat@users.noreply.github.com>
2024-08-21 14:28:17 +0200

    so selecting a tmux or a neovim pane can be done using the same bindings

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -471,6 +471,19 @@ plugins = {
       -- refer to the configuration section below
     },
   },
+  {
+    "mrjones2014/smart-splits.nvim",
+    config = function()
+      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+      vim.keymap.set("n", "<A-h>", require("smart-splits").move_cursor_left)
+      vim.keymap.set("n", "<A-j>", require("smart-splits").move_cursor_down)
+      vim.keymap.set("n", "<A-k>", require("smart-splits").move_cursor_up)
+      vim.keymap.set("n", "<A-l>", require("smart-splits").move_cursor_right)
+    end,
+  },
 }
 opts = {
   dev = {
