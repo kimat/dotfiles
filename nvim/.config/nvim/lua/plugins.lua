8ef513cec3eea4f5e07bc92c8ad4ae4fde3574e4
Kimat Boven <kimat@users.noreply.github.com>
2024-09-09 22:30:56 +0200

    autocompletion suggestions were used even when not desired

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -283,7 +283,7 @@ local plugins = {
       local cmp = require "cmp"
       cmp.setup {
         mapping = cmp.mapping.preset.insert {
-          ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         },
         formatting = {
           format = function(entry, vim_item)
