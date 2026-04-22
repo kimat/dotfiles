b0de05266346a7b037a68431a17085cfe4483710
Kimat Boven <kimat@users.noreply.github.com>
2026-04-22 17:23:47 +0200

    was getting with an error without this now

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -54,6 +54,7 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
       require("nvim-treesitter.configs").setup {
         textobjects = {
